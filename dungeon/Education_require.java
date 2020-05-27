ddf2debfb8de6a19535617a176950736f557841f
HanulChoe <21500761@handong.edu>
2020-05-27 19:04:30 +0900

    요구사항 수정, 죽은몬스터 오류 수정, 코드정리

--- dungeon/Education_require.java
+++ dungeon/Education_require.java
@@ -9,36 +9,64 @@ public class Education_require extends Dungeon {
 	public Education_require(String name) {
 		super(name);
 	}
-	
-	public void fight(Me me,Skill mySkill[]) {
+
+	public void fight(Me me, Skill mySkill[]) {
+
+		System.out.println("Enter the dungeon : Handong Insung GyeoYook");
+
 		Monster[] monsters = new Monster[5];
-		monsters[0] = new Monster("한인교 소감문",3,1,5,1,10);
-		monsters[1] = new Monster("한인교 소감문",3,1,5,1,10);
-		monsters[2] = new Monster("한인교 소감문",3,1,5,1,10);
-		monsters[3] = new Monster("한인교 소감문",3,1,5,1,10);
-		monsters[4] = new Monster("한인교 소감문",3,1,5,1,10);
+		monsters[0] = new Monster("한인교 소감문", 3, 1, 5, 1, 10);
+		monsters[1] = new Monster("한인교 소감문", 3, 1, 5, 1, 10);
+		monsters[2] = new Monster("한인교 소감문", 3, 1, 5, 1, 10);
+		monsters[3] = new Monster("한인교 소감문", 3, 1, 5, 1, 10);
+		monsters[4] = new Monster("한인교 소감문", 3, 1, 5, 1, 10);
 
-		
-		
-		while(me.getHp()>0) {
-			System.out.println("내 체력 :"+me.getHp());
-			for(int i = 0 ; i < monsters.length; i++) {
-			System.out.println(i+" "+monsters[i].getNmae()+" : "+ monsters[i].getHp());
-			}
-			me.Attack(mySkill,monsters);
-			int i;
-			for(i = 0; i < monsters.length; i++) {
-				if(monsters[i].getHp()>0)break;
-			}
-			if(i == monsters.length) return;
-			
-			
-			monsters[0].Attack(me);
-			monsters[1].Attack(me);
-			monsters[2].Attack(me);
+		for (int i = 0; i < monsters.length; i++) {
+			System.out.println("야생의 " + i + " " + monsters[0].getName() + " 몬스터가 나타났다!");
 		}
-		
-		
-	}
 
+		while (me.getHp() > 0) {
+
+			System.out.println();
+			System.out.println("[My status]");
+			System.out.println("HP :" + me.getHp());
+			System.out.println("MP :" + me.getMp());
+			System.out.println();
+
+			System.out.println("[Monsters status]");
+			for (int i = 0; i < monsters.length; i++) {
+				if (monsters[i].getHp() < 0) {
+					System.out.println(i + " " + monsters[i].getName() + ": 0");
+					continue;
+				}
+				System.out.println(i + " " + monsters[i].getName() + " : " + monsters[i].getHp());
+			}
+			System.out.println();
+			me.Attack(mySkill, monsters, me);
+
+			// Clear condition
+			int i;
+			for (i = 0; i < monsters.length; i++) {
+				if (monsters[i].getHp() > 0)
+					break;
+			}
+			if (i == monsters.length) {
+				System.out.println("Dungeon Clear!");
+				System.out.println();
+				return;
+			}
+
+			System.out.println();
+			for (int j = 0; j < monsters.length; j++) {
+				if (monsters[j].getHp() > 0) {
+					System.out.println(monsters[j].getName() + j + " 에게 공격받았다!.");
+					System.out.println(me.getDef() - monsters[j].getAtk());
+					monsters[j].Attack(me, monsters[j]);
+				}
+			}
+			if (me.getHp() > 0) {
+				me.set_clear_education_require(true);
+			}
+		}
+	}
 }
--- dungeon/Major_require.java
+++ dungeon/Major_require.java
@@ -1,36 +1,70 @@
 package dungeon;
+
 import character.*;
 import skill.Skill;
 
 public class Major_require extends Dungeon {
-	
+
 	public Major_require(String name) {
 		super(name);
 	}
-
-	private String name;
+	
 	private int grade = 1;
 
-	public void fight(Me me,Skill mySkill[]) {
+	public void fight(Me me, Skill mySkill[]) {
+		
+		System.out.println("Enter the dungeon. : Introductory Engineering");
+
 		Monster[] monsters = new Monster[3];
-		monsters[0] = new Monster("C프로그래밍 퀴즈",10,1,5,1,30);
-		monsters[1] = new Monster("C프로그래밍 퀴즈",10,1,5,1,30);
-		monsters[2] = new Monster("C프로그래밍 퀴즈",10,1,5,1,30);
+		monsters[0] = new Monster("보고서", 10, 1, 5, 1, 30);
+		monsters[1] = new Monster("Line tracer", 10, 1, 5, 1, 30);
+		monsters[2] = new Monster("Wrestling robot", 10, 1, 5, 1, 30);
 		
+		System.out.println("야생의"+ monsters[0].getName() +" 몬스터가 나타났다!");
+		System.out.println("야생의"+ monsters[1].getName() +" 몬스터가 나타났다!");
+		System.out.println("야생의"+ monsters[2].getName() +" 몬스터가 나타났다!");
 		
-		while((monsters[0].getHp()>0||monsters[1].getHp()>0||monsters[2].getHp()>0)&&me.getHp()>0) {
-			System.out.println("내 체력 :"+me.getHp());
-			for(int i = 0 ; i < monsters.length; i++) {
-			System.out.println(i+" "+monsters[i].getNmae()+" : "+ monsters[i].getHp());
+		// Battle
+		while ((monsters[0].getHp() > 0 || monsters[1].getHp() > 0 || monsters[2].getHp() > 0) && me.getHp() > 0) {
+
+			System.out.println();
+			System.out.println("[My status]");
+			System.out.println("HP :" + me.getHp());
+			System.out.println("MP :" + me.getMp());
+			System.out.println();
+			
+			// Monster status
+			System.out.println("[Monsters status]");
+			for (int i = 0; i < monsters.length; i++) {
+				System.out.println(i + " " + monsters[i].getName() + " Hp : " + monsters[i].getHp());
 			}
-			me.Attack(mySkill,monsters);
-			monsters[0].Attack(me);
-			monsters[1].Attack(me);
-			monsters[2].Attack(me);
+			
+			System.out.println();
+			me.Attack(mySkill, monsters, me);
+			System.out.println();
+			
+			// Clear condition
+			int i;
+			for (i = 0; i < monsters.length; i++) {
+				if (monsters[i].getHp() > 0)
+					break;
+			}
+			if (i == monsters.length) {
+				System.out.println("Dungeon Clear!");
+				System.out.println();
+				return;
+			}
+			
+			// Be attacked by monsters
+			for (int j = 0; j < 3; j++) {
+				if (monsters[j].getHp() > 0) {
+					System.out.println(monsters[j].getName() + j + " 에게 공격받았다!." + (me.getDef() - monsters[j].getAtk()));
+					monsters[j].Attack(me, monsters[j]);
+				}
+			}
+			if (me.getHp() > 0) {
+				me.set_clear_major_require(true);
+			}
 		}
-		if(me.getHp()>0) {
-			me.set_clear_major_require(true);
-		}
-		
 	}
 }
