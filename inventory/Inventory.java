a323f6f81f95e88f8aa39b407dbc96a16802e5e4
21700340JuwonBaek <21700340@handong.edu>
2020-05-29 22:57:35 +0900

    inventory 저장이 되도록 업데이트

--- inventory/Inventory.java
+++ inventory/Inventory.java
@@ -24,6 +24,51 @@ public class Inventory {
 		this.MpPotion = MpPotion;
 	}
 	
+	public void getInventory(Weapon weapon1, Weapon weapon2, Weapon weapon3, Armor armor1, Armor armor2, Armor armor3, Potion HpPotion, Potion MpPotion) {
+		weapon1 = this.weapon1;
+		weapon2 = this.weapon2;
+		weapon3 = this.weapon3;
+		armor1 = this.armor1;
+		armor2 = this.armor2;
+		armor3 = this.armor3;
+		HpPotion = this.HpPotion;
+		MpPotion = this.MpPotion;
+	}
+	//저장된 파일을 불러왔을 때에 instatnceVariable에 값이 저장되어 있을테니,
+	//get함수를 통해서 parameter들에게 저장된 값을 할당하려는 의도. 잘 될런지는 모르겠다. 
+	
+	public void setWeapon1(Weapon weapon1) {
+		this.weapon1 = weapon1;
+	}
+	//아이템을 판매하거나 구매했을 때 혹은 아이템을 사용했을 때에 set함수를 사용한다. 
+	
+	public void setWeapon2(Weapon weapon2) {
+		this.weapon1 = weapon1;
+	}
+	
+	public void setWeapon3(Weapon weapon3) {
+		this.weapon1 = weapon1;
+	}
+	
+	public void setArmor1(Armor armor) {
+		this.armor1 = armor;
+	}
+	
+	public void setArmor2(Armor armor) {
+		this.armor2 = armor;
+	}
+	
+	public void setArmor3(Armor armor) {
+		this.armor3 = armor;
+	}
+	
+	public void setHpPotion(Potion HpPotion) {
+		this.HpPotion = HpPotion;
+	}
+	
+	public void setMpPotion(Potion MpPotion) {
+		this.MpPotion = MpPotion;
+	}
 	
 	
 	public static void detatchArmor(Me me, Armor armor) {
