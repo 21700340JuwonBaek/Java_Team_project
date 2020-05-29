e44744af50a43f2511823368dc837ceace75a6d2
21700340JuwonBaek <21700340@handong.edu>
2020-05-29 22:13:15 +0900

    방어구의 객체에서 추가 체력변수를 선언

--- item/Armor.java
+++ item/Armor.java
@@ -2,16 +2,21 @@ package item;
 
 public class Armor extends Item{
 private int def;
+private int hp;
 private boolean is_bought;
-	public Armor(String name, int buy_gold, int sell_gold, int grade, int def) {
+	public Armor(String name, int buy_gold, int sell_gold, int grade, int def, int hp) {
 		super(name, buy_gold, sell_gold, grade);
-		// TODO Auto-generated constructor stub
 		this.def = def;
+		this.hp = hp;
 		this.is_bought = false;
 	}
 	
 	public int getDef() {
 		return this.def;
+	}
+	
+	public int getHp() {
+		return this.hp;
 	}
 	
 	public boolean getIs_bought() {
