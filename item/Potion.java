51be418184a1a91419c674bfcab0050600236550
21700340JuwonBaek <21700340@handong.edu>
2020-06-02 12:04:13 +0900

    생성자파라미터에 포션의 개수를 설정할 수 있도록 설정

--- item/Potion.java
+++ item/Potion.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 public class Potion extends Item implements Serializable{
 	private int recovory;
 	private int number;//포션의 개수.
-	public Potion(String name, int buy_gold, int sell_gold, int grade, int recovory) {
+	public Potion(String name, int buy_gold, int sell_gold, int grade, int recovory, int number) {
 		super(name, buy_gold, sell_gold, grade);
 		this.recovory = recovory;
-		this.number = 0;
+		this.number = number;
 	}
 	
 	public int getRecovory() {
