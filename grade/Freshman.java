b2669fac0b2528de53b21f231173a992c0cb0ec0
HanulChoe <21500761@handong.edu>
2020-06-03 03:50:05 +0900

    몬스터 이름 변경, HP, 골드 변경, 뒤로가기 추가

--- grade/Freshman.java
+++ grade/Freshman.java
@@ -1,4 +1,5 @@
 package grade;
+
 import skill.Skill;
 import dungeon.*;
 import inventory.Inventory;
@@ -8,85 +9,88 @@ import character.Me;
 import character.Monster;
 
 public class Freshman {
-	
-	public static Skill[] skill = new Skill[5];{
-	skill[0] = new Skill("검색하기",5,1,3,true);//Skill name, Damage, Target, ManaSpent, Open
-	skill[1] = new Skill("우주의 기원을 담아 기도하기...",5,4,10,true);
-	skill[2] = new Skill("선배님 도와주세요...!",10,1,10,true);
-	skill[3] = new Skill("논문찾아보기",20,1,15,false);
-	skill[4] = new Skill("교수님께 메일보내보기",30,1,20,false);
 
+	public static Skill[] skill = new Skill[5];
+	{
+		// Skill name, Attack point, Target, ManaSpent, Open
+		skill[0] = new Skill("검색하기", 5, 1, 3, true);
+		skill[1] = new Skill("우주의 기원을 담아 기도하기...", 5, 4, 10, true);
+		skill[2] = new Skill("선배님 도와주세요...!", 10, 1, 10, true);
+		skill[3] = new Skill("논문찾아보기", 20, 1, 15, false);
+		skill[4] = new Skill("교수님께 메일보내보기", 30, 1, 20, false);
 	}
-	
-	public final static Monster[] ER = new Monster[3]; {
-	
-	ER[0] = new Monster("1학년교필 서평",10,1,5,1,60 );
-	ER[1] = new Monster("1학년교필중간고사",10,1,5,1,60 );
-	ER[2] = new Monster("1학년교필기말고사",10,1,5,1,60 );
+
+	public final static Monster[] MR = new Monster[3];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		MR[0] = new Monster("라인 트레이서", 3, 1, 5, 1, 30);
+		MR[1] = new Monster("공설입 중간고사", 5, 1, 5, 1, 50);
+		MR[2] = new Monster("공설입 기말고사", 5, 1, 5, 1, 50);
 	}
-	
-	
-	public final static Monster[]MR = new Monster[3];{
-	MR[0] = new Monster("1학년전필코딩과제",1,1,5,1,60 );
-	MR[1] = new Monster("1학년전필중간고사",1,1,5,1,60 );
-	MR[2] = new Monster("1학년전필기말고사",1,1,5,1,60 );
+
+	public final static Monster[] ER = new Monster[3];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		ER[0] = new Monster("한인교 보고서", 3, 1, 5, 1, 30);
+		ER[1] = new Monster("한인교 보고서", 3, 1, 5, 1, 30);
+		ER[2] = new Monster("한인교 보고서", 3, 1, 5, 1, 30);
 	}
-	
-	public final static Monster[] ES = new Monster[3]; {
-		
-	ES[0] = new Monster("1학년교선서평",1,1,5,1,60 );
-	ES[1] = new Monster("1학년교션중간고사",1,1,5,1,60 );
-	ES[2] = new Monster("1학년교선기말고사",1,1,5,1,60 );
+
+	public final static Monster[] MS = new Monster[3];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		MS[0] = new Monster("코딩과제", 3, 1, 5, 1, 30);
+		MS[1] = new Monster("파이썬 중간고사", 5, 1, 5, 1, 50);
+		MS[2] = new Monster("파이썬 기말고사", 5, 1, 5, 1, 50);
 	}
-	
-	public final static Monster[]MS = new Monster[3];{
-	MS[0] = new Monster("1학년전선코딩과제",1,1,5,1,60 );
-	MS[1] = new Monster("1학년전선중간고사",1,1,5,1,60 );
-	MS[2] = new Monster("1학년전선기말고사",1,1,5,1,60 );
+
+	public final static Monster[] ES = new Monster[3];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		ES[0] = new Monster("F=ma", 3, 1, 5, 1, 20);
+		ES[1] = new Monster("상대성 이론", 5, 1, 5, 1, 40);
+		ES[2] = new Monster("베르누이 법칙", 5, 1, 5, 1, 40);
 	}
-	
-	
-	protected static Education_require  Education_require;
+
+	protected static Education_require Education_require;
 	protected static Major_require Major_require;
 	protected static Education_select Education_select;
 	protected static Major_select Major_select;
-	//만약 학년별로 각 던전을 상속받는 클래스가 나온다면, 생성자에 int형 변수 파라미터를 추가해서.... if문으로 해야할 것 같다. 
-	public Freshman(String major1, String major2 , String education1, String education2) {
-		this.Education_require = new Education_require(education1);
-		this.Major_require = new Major_require(major1);
-		this.Education_select = new Education_select(education2);
-		this.Major_select = new Major_select(major2);
-		
+
+	// 만약 학년별로 각 던전을 상속받는 클래스가 나온다면, 생성자에 int형 변수 파라미터를 추가해서.... if문으로 해야할 것 같다.
+	public Freshman(String major1, String major2, String education1, String education2) {
+		Freshman.Education_require = new Education_require(education1);
+		Freshman.Major_require = new Major_require(major1);
+		Freshman.Education_select = new Education_select(education2);
+		Freshman.Major_select = new Major_select(major2);
 	}
-	public static void GoToDungeon(Me me,Inventory invent){
 
-		
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please Select the Dungeon\n\n1."+Major_require.getName());
-		System.out.println("2."+Education_require.getName());
-		System.out.println("3."+Major_select.getName());
-		System.out.println("4."+Education_select.getName());
+	public static void GoToDungeon(Me me, Inventory invent) {
 
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("던전 선택\n1." + Major_require.getName());
+			System.out.println("2." + Education_require.getName());
+			System.out.println("3." + Major_select.getName());
+			System.out.println("4." + Education_select.getName());
+			System.out.println("5. 뒤로가기");
+			int select = sc.nextInt();
 
-		int select = sc.nextInt();
-		
-		if(select == 1) {
-			Major_require.fight(me, skill, MR, invent);
+			if (select == 1) {
+				Major_require.fight(me, skill, MR, invent);
+			}
+			else if (select == 2) {
+				Education_require.fight(me, skill, ER, invent);
+			}
+			else if (select == 3) {
+				Major_select.fight(me, skill, MS, invent);
+			}
+			else if (select == 4) {
+				Education_select.fight(me, skill, ES, invent);
+			}
+			else if (select == 5) {
+				break;
+			}
 		}
-		if(select == 2) {
-			Education_require.fight(me, skill, ER, invent);
-		}
-		if(select == 3) {
-			Major_select.fight(me, skill, MS, invent);
-		}
-		if(select == 4) {
-			Education_select.fight(me, skill, ES, invent);
-		}
-		
 	}
-	
-	
-	
-
 }
--- grade/Junior.java
+++ grade/Junior.java
@@ -6,78 +6,70 @@ import character.Me;
 import character.Monster;
 import inventory.Inventory;
 
-public class Junior extends Sophomore{
-	
-	
-		
-		public final static Monster[] ER3 = new Monster[4]; {
-			
-			ER3[0] = new Monster("3학년교필 서평",1,1,5,1,6 );
-			ER3[1] = new Monster("3학년교필중간고사",1,1,5,1,6 );
-			ER3[2] = new Monster("3학년교필기말고사",1,1,5,1,6 );
-			ER3[3] = new Monster("3학년교필기말고사",1,1,5,1,6 );
+public class Junior extends Sophomore {
 
-			}
-			
-			
-			public final static Monster[]MR3 = new Monster[4];{
-			MR3[0] = new Monster("3학년전필코딩과제",1,1,5,1,6 );
-			MR3[1] = new Monster("3학년전필중간고사",1,1,5,1,6 );
-			MR3[2] = new Monster("3학년전필기말고사",1,1,5,1,6 );
-			MR3[3] = new Monster("3학년교필기말고사",1,1,5,1,6 );
+	public final static Monster[] MR3 = new Monster[4];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		MR3[0] = new Monster("3학년전필코딩과제", 4, 1, 5, 1, 40);
+		MR3[1] = new Monster("3학년전필중간고사", 5, 1, 5, 1, 45);
+		MR3[2] = new Monster("3학년전필기말고사", 5, 1, 5, 1, 60);
+		MR3[3] = new Monster("3학년교필기말고사", 5, 1, 5, 1, 60);
+	}
 
-			}
-			
-			public final static Monster[] ES3 = new Monster[4]; {
-				
-			ES3[0] = new Monster("3학년교선서평",1,1,5,1,6 );
-			ES3[1] = new Monster("3학년교션중간고사",1,1,5,1,6 );
-			ES3[2] = new Monster("3학년교선기말고사",1,1,5,1,6 );
-			ES3[3] = new Monster("3학년교필기말고사",1,1,5,1,6 );
+	public final static Monster[] ER3 = new Monster[4];
+	{
+		// Skill name, Attack point, Target, ManaSpent, Open
+		ER3[0] = new Monster("3학년교필 서평", 3, 1, 5, 1, 40);
+		ER3[1] = new Monster("3학년교필 보고서", 5, 1, 5, 1, 45);
+		ER3[2] = new Monster("3학년교필기말고사", 5, 1, 5, 1, 50);
+		ER3[3] = new Monster("3학년교필기말고사", 5, 1, 5, 1, 50);
+	}
 
-			}
-			
-			public final static Monster[]MS3 = new Monster[4];{
-			MS3[0] = new Monster("3학년전선코딩과제",1,1,5,1,6 );
-			MS3[1] = new Monster("3학년전선중간고사",1,1,5,1,6 );
-			MS3[2] = new Monster("3학년전선기말고사",1,1,5,1,6 );
-			MS3[3] = new Monster("3학년교필기말고사",1,1,5,1,6 );
+	public final static Monster[] MS3 = new Monster[4];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		MS3[0] = new Monster("3학년전선코딩과제", 4, 1, 5, 1, 30);
+		MS3[1] = new Monster("3학년전선중간고사", 5, 1, 5, 1, 40);
+		MS3[2] = new Monster("3학년전선기말고사", 5, 1, 5, 1, 60);
+		MS3[3] = new Monster("3학년교필기말고사", 5, 1, 5, 1, 60);
+	}
 
-			}
-			
+	public final static Monster[] ES3 = new Monster[4];
+	{
+		// Name, Attack point, Defense point, gold, critical, Hp
+		ES3[0] = new Monster("3학년교선서평", 3, 1, 5, 1, 30);
+		ES3[1] = new Monster("3학년교션중간고사", 5, 1, 5, 1, 40);
+		ES3[2] = new Monster("3학년교선기말고사", 5, 1, 5, 1, 50);
+		ES3[3] = new Monster("3학년교필기말고사", 5, 1, 5, 1, 50);
+	}
 
 	public Junior(String major1, String major2, String education1, String eucation2) {
 		super(major1, major2, education1, eucation2);
 		skill[4].setOpen(true);
 	}
-	
-	public static void GoToDungeon(Me me, Inventory invent){
 
-		
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please Select the Dungeon\n\n1."+Major_require.getName());
-		System.out.println("2."+Education_require.getName());
-		System.out.println("3."+Major_select.getName());
-		System.out.println("4."+Education_select.getName());
+	public static void GoToDungeon(Me me, Inventory invent) {
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Please Select the Dungeon\n\n1." + Major_require.getName());
+			System.out.println("2." + Education_require.getName());
+			System.out.println("3." + Major_select.getName());
+			System.out.println("4." + Education_select.getName());
 
+			int select = sc.nextInt();
 
-		int select = sc.nextInt();
-		
-		if(select == 1) {
-			Major_require.fight(me, skill, MR3, invent);
+			if (select == 1) {
+				Major_require.fight(me, skill, MR3, invent);
+			} else if (select == 2) {
+				Education_require.fight(me, skill, ER3, invent);
+			} else if (select == 3) {
+				Major_select.fight(me, skill, MS3, invent);
+			} else if (select == 4) {
+				Education_select.fight(me, skill, ES3, invent);
+			} else if (select == 5) {
+				break;
+			}
 		}
-		if(select == 2) {
-			Education_require.fight(me, skill, ER3, invent);
-		}
-		if(select == 3) {
-			Major_select.fight(me, skill, MS3, invent);
-		}
-		if(select == 4) {
-			Education_select.fight(me, skill, ES3, invent);
-		}
-		
 	}
-	
-	
 }
