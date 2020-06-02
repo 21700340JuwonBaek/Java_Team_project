6bb5a44c8675e7fa2ee209c71acdb7a593f0cd44
Yeonhee77 <61729546+Yeonhee77@users.noreply.github.com>
2020-06-02 18:48:45 +0900

    아이템 구매 후 인벤토리에 적용(테스트)

--- shop/shop.java
+++ shop/shop.java
@@ -2,6 +2,10 @@ package shop;
 
 import java.util.Scanner;
 import character.Me;
+import inventory.Inventory;
+import shop.shop;
+import item.*;
+import item.Potion;
 
 public class shop {
 	
@@ -22,8 +26,15 @@ public class shop {
 				System.out.println("0. 취소\n1. HP 포션: 체력을 최대로 회복시켜준다.(10 골드)\n2. MP 포션: 마나를 최대로 회복시켜준다.(10 골드)");
 				item = sc.nextInt();
 				
-				if(item != 0) me.setGold(me.getGold()-10);
-				else System.out.println("그럼 다른 아이템을 구매 하실건가요?");
+				if(item == 1) {
+					System.out.println("HP 포션을 구매하였습니다! 10골드 차감.");
+					me.setGold(me.getGold()-10);
+					Potion hp = new Potion("HP Potion",10,10,0,100,1);
+				}else if(item==2) {
+					System.out.println("MP 포션을 구매하였습니다! 10골드 차감.");
+					me.setGold(me.getGold()-10);
+					Potion mp = new Potion("Mp Potion",10,10,0,100,1);
+				}else System.out.println("그럼 다른 아이템을 구매 하실건가요?");
 			}else if(kind==2) {
 				System.out.println("구매하려는 아이템을 골라주세요! ");
 				System.out.println("0. 취소\n1. 아이패드: 공격력 5 증가(20골드)\n2. 새내기 후드티: 방어력 5 증가(20골드)\n3. 맥북: 공격력 7 증가(30골드)\n4. 선배의 필기노트: 공격력 10 증가(50골드)\n5. TA 세션: 공격력 12 증가(60골드)");
@@ -34,21 +45,27 @@ public class shop {
 				}else if(item==1) {
 					System.out.println("아이패드를 구매하였습니다. 공격력 +5 ");
 					me.setGold(me.getGold()-20);
+					Weapon Ipad = new Weapon("Ipad", 100,100,1,10,10);
 				}else if(item==2) {
 					System.out.println("새내기 후드티를 구매하였습니다. 방어력 +5 ");
 					me.setGold(me.getGold()-20);
+					Armor Hood = new Armor("Hood", 100,100,1,1,10);
 				}else if(item==3) {
 					System.out.println("맥북을 구매하였습니다. 공격력 +7 ");
 					me.setGold(me.getGold()-30);
+					Weapon Macbook = new Weapon("Macbook", 100,100,1,10,10);
 				}else if(item==4) {
 					System.out.println("선배의 필기노트를 구매하였습니다. 공격력 +10 ");
 					me.setGold(me.getGold()-50);
+					Weapon Note = new Weapon("Note", 100,100,1,10,10);
 				}else if(item==5) {
 					System.out.println("TA 세션을 들었습니다. 공격력 +12 ");
 					me.setGold(me.getGold()-60);
+					Weapon TA = new Weapon("TA", 100,100,1,10,10);
 				}
 			}
 		}while(kind!=0);
+		
 		System.out.println("안녕히가세요!");	
-	}
+	
 }
