cdbd1bd8495bc7561021b52b9fe2519f59efca50
Yeonhee77 <61729546+Yeonhee77@users.noreply.github.com>
2020-06-02 19:36:40 +0900

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -75,8 +75,8 @@ public class Main {
    public static void main (String[] args) {
 	   //Inventory inventory = new Inventory
       
-      Me me = new Me("이름",10,1,500,1,100,100);
-      //이름/공격력/방어력/소지골드/크리티컬/최대체력/최대마나 입니다
+      Me me = new Me("이름",10,1,500,1,100,100,100,100);
+      //이름/공격력/방어력/소지골드/크리티컬/체력/마나/최대체력/최대마나 입니다
 	   Potion hp = new Potion("HP Potion",10,10,0,100,1);
 	   Potion mp = new Potion("Mp Potion",10,10,0,100,1);
 	   Weapon Ipad = new Weapon("Ipad", 100,100,1,10,10);
