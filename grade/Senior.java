60f7ea588fe1c5618d9c08665cc67f4bf17b498f
21700340JuwonBaek <21700340@handong.edu>
2020-06-02 23:12:58 +0900

    몬스터 수 변경

--- grade/Senior.java
+++ grade/Senior.java
@@ -7,32 +7,45 @@ import character.Monster;
 import inventory.Inventory;
 
 public class Senior extends Junior {
-	public final static Monster[] ER4 = new Monster[3];
+	public final static Monster[] ER4 = new Monster[5];
 	{
-		ER4[0] = new Monster("4학년교필 서평",1,1,5,1,60 );
-		ER4[1] = new Monster("4학년교필중간고사",1,1,5,1,60 );
-		ER4[2] = new Monster("4학년교필기말고사",1,1,5,1,60 );
+		ER4[0] = new Monster("인자기",1,1,5,1,1 );
+		ER4[1] = new Monster("졸업영어",1,1,5,1,1 );
+		ER4[2] = new Monster("서평1",1,1,5,1,1 );
+		ER4[3] = new Monster("레포트",1,1,5,1,1 );
+		ER4[4] = new Monster("서평2",1,1,5,1,1 );
+
 		}
 		
 		
-		public final static Monster[]MR4 = new Monster[3];{
-		MR4[0] = new Monster("4학년전필코딩과제",1,1,5,1,60 );
-		MR4[1] = new Monster("4학년전필중간고사",1,1,5,1,60 );
-		MR4[2] = new Monster("4학년전필기말고사",1,1,5,1,60 );
+		public final static Monster[]MR4 = new Monster[5];{
+		MR4[0] = new Monster("졸업논문1",1,1,5,1,60 );
+		MR4[1] = new Monster("캡스톤",1,1,5,1,60 );
+		MR4[2] = new Monster("팀프로젝트",1,1,5,1,60 );
+		MR4[3] = new Monster("취업준비",1,1,5,1,60 );
+		MR4[4] = new Monster("졸업논문2",1,1,5,1,60 );
+
 		}
 		
-		public final static Monster[] ES4 = new Monster[3]; {
+		public final static Monster[] ES4 = new Monster[5];
+		{
+			ES4[0] = new Monster("인자기",1,1,5,1,60 );
+			ES4[1] = new Monster("졸업영어",1,1,5,1,60 );
+			ES4[2] = new Monster("서평1",1,1,5,1,60 );
+			ES4[3] = new Monster("레포트",1,1,5,1,60 );
+			ES4[4] = new Monster("서평2",1,1,5,1,60 );
+
+			}
 			
-		ES4[0] = new Monster("4학년교선서평",1,1,5,1,60 );
-		ES4[1] = new Monster("4학년교션중간고사",1,1,5,1,60 );
-		ES4[2] = new Monster("4학년교선기말고사",1,1,5,1,60 );
-		}
-		
-		public final static Monster[]MS4 = new Monster[3];{
-		MS4[0] = new Monster("4학년전선코딩과제",1,1,5,1,60 );
-		MS4[1] = new Monster("4학년전선중간고사",1,1,5,1,60 );
-		MS4[2] = new Monster("4학년전선기말고사",1,1,5,1,60 );
-		}
+			
+			public final static Monster[]MS4 = new Monster[5];{
+			MS4[0] = new Monster("졸업논문1",1,1,5,1,60 );
+			MS4[1] = new Monster("캡스톤",1,1,5,1,60 );
+			MS4[2] = new Monster("팀프로젝트",1,1,5,1,60 );
+			MS4[3] = new Monster("취업준비",1,1,5,1,60 );
+			MS4[4] = new Monster("졸업논문2",1,1,5,1,60 );
+
+			}
 		
 	public Senior(String major1, String major2, String education1, String eucation2) {
 		super(major1, major2, education1, eucation2);
@@ -48,6 +61,7 @@ public static void GoToDungeon(Me me, Inventory invent){
 		System.out.println("4."+Education_select.getName());
 
 
+
 		int select = sc.nextInt();
 		
 		if(select == 1) {
@@ -56,13 +70,14 @@ public static void GoToDungeon(Me me, Inventory invent){
 		if(select == 2) {
 			Education_require.fight(me, skill, ER4, invent);
 		}
+		
 		if(select == 3) {
 			Major_select.fight(me, skill, MS4, invent);
 		}
 		if(select == 4) {
 			Education_select.fight(me, skill, ES4, invent);
 		}
-		
+
 	}
 	
 	
